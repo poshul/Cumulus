684b45f0f82b68fdb75ed564b78f52af105de22e
samuel <samuel@B-tech-sam>
2011-12-02 09:20:38 -0500

    Initializer first draft finished

--- com.btechconsulting.wein.cumulus/src/com/btechconsulting/wein/cumulus/initialization/Constants.java
+++ com.btechconsulting.wein.cumulus/src/com/btechconsulting/wein/cumulus/initialization/Constants.java
@@ -5,4 +5,9 @@ public class Constants {
 	public static final Integer initialInstances=2;
 	public static final String dispatchQueueName="dispatchQueue";
 	public static final String returnQueueName="returnQueue";
+	public static final String ec2Region="ec2.us-east-1d.amazonaws.com";
+	public static final String instanceType="a1.micro";//"c1.xlarge"; TODO switch back
+	public static final String imageID="ami-1e39ca77";
+	public static final String securityGroupID="ssh";
+	public static final String keyName="Samkeys";
 }
--- com.btechconsulting.wein.cumulus/src/com/btechconsulting/wein/cumulus/initialization/Initializer.java
+++ com.btechconsulting.wein.cumulus/src/com/btechconsulting/wein/cumulus/initialization/Initializer.java
@@ -6,10 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
@@ -20,39 +23,70 @@ import com.amazonaws.services.sqs.model.CreateQueueRequest;
  *
  */
 public enum Initializer {
-	instance;
+	INSTANCE;
+	
 
 	public enum wUStatus{
 		INFLIGHT,DONE,ERROR
 	}
-
+	private AmazonSQS dispatchQueue;
+	private AmazonSQS returnQueue;
+	private Map<String, Map<String,wUStatus>> unitsOnServer;
+	
 	private Initializer(){
 		try{
-			AmazonSQS dispatchQueue = createQueue(Constants.credentialsFile, Constants.dispatchQueueName);
-			AmazonSQS returnQueue = createQueue(Constants.credentialsFile, Constants.returnQueueName);
-			Map<String, Map<String,wUStatus>> unitsOnServer= createUnitsOnServer();
-			createInitialInstances(Constants.initialInstances, Constants.credentialsFile);
+			dispatchQueue = createQueue(Constants.credentialsFile, Constants.dispatchQueueName);
+			returnQueue = createQueue(Constants.credentialsFile, Constants.returnQueueName);
+			unitsOnServer= createUnitsOnServer();
+			createInitialInstances(Constants.credentialsFile);
 		}
-		catch (Exception e) {
-			// TODO: handle exception
+		catch (AmazonServiceException ase) {
+			System.err.println("Caught an AmazonServiceException, which means your request made it " +
+					"to Amazon AWS, but was rejected with an error response for some reason.");
+			System.err.println("Error Message:    " + ase.getMessage());
+			System.err.println("HTTP Status Code: " + ase.getStatusCode());
+			System.err.println("AWS Error Code:   " + ase.getErrorCode());
+			System.err.println("Error Type:       " + ase.getErrorType());
+			System.err.println("Request ID:       " + ase.getRequestId());
+		}
+		catch (AmazonClientException ace) {
+            System.err.println("Caught an AmazonClientException, which means the client encountered " +
+                    "a serious internal problem while trying to communicate with AWS, such as not " +
+                    "being able to access the network.");
+            System.err.println("Error Message: " + ace.getMessage());
+		}
+		catch (Exception e){
+			System.err.println();
 		}
 
 	}
 
-	private void createInitialInstances(Integer initialinstances, String credentialsFile) throws Exception {
+	private void createInitialInstances(String credentialsFile) throws Exception {
 		AmazonEC2 ec2= new AmazonEC2Client(new PropertiesCredentials(
 				Initializer.class.getResourceAsStream(credentialsFile)));
-		//TODO restart here
-		
+		//set the zone
+		ec2.setEndpoint(Constants.ec2Region);
+		System.out.println("Intializing "+Constants.initialInstances+" instances\n");
+		//create EC2 instances
+		RunInstancesRequest runInstancesRequest = new RunInstancesRequest()
+		.withInstanceType(Constants.instanceType)
+		.withImageId(Constants.imageID)
+		.withMinCount(Constants.initialInstances)
+		.withMaxCount(Constants.initialInstances)
+		.withSecurityGroupIds(Constants.securityGroupID)
+		.withKeyName(Constants.keyName);
+
+		RunInstancesResult runInstances = ec2.runInstances(runInstancesRequest);
+		System.out.println(runInstances.toString());
 	}
 
 	/* creatUnitsOnServer: creates the datastructure for storing workunits
 	 */
-	
+
 	private Map<String, Map<String, wUStatus>> createUnitsOnServer() {
-		 return Collections.synchronizedMap(new HashMap<String, Map<String,wUStatus>>());
+		return Collections.synchronizedMap(new HashMap<String, Map<String,wUStatus>>());
 	}
-	
+
 	/* createQueue: creates the AWS SQS queue for cumulus.  Throws Exception if we can't create the queue
 	 * @param credentialsFile the file containing credentials to access the AWS
 	 * @return AmazonSQS the queue
@@ -65,5 +99,5 @@ public enum Initializer {
 		CreateQueueRequest createQueueRequest = new CreateQueueRequest(queueName);
 		return sqs;
 	}
-	
+
 }
